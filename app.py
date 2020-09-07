7217046b5524ff270d81ccbe9e267d99e5c6aa2b
wade <wadechen@outlook.com>
2020-09-07 21:02:11 +0800

    add module b

--- app.py
+++ app.py
@@ -76,5 +76,51 @@ def a():
                      as_attachment=True)
 
 
+@app.route('/b', methods=['POST'])
+def b():
+    pdfname = request.files.get('originFileB').filename
+    pdfpathname = os.path.join(os.getcwd(), pdfname)
+    namepre = os.path.splitext(pdfname)[0]
+
+    imagepath = os.path.join(os.getcwd(), namepre)
+    zipfilename = namepre + '.zip'
+    zippath = os.path.join(os.getcwd(), 'zipfiles')
+    zippathname = os.path.join(zippath, zipfilename)
+
+    request.files.get('originFileB').save(pdfpathname)
+
+    if os.path.exists(zippath):
+        shutil.rmtree(zippath)
+    os.makedirs(zippath)
+    if not os.path.exists(imagepath):
+        os.makedirs(imagepath)
+
+    filedoc = fitz.open(pdfpathname)
+    for pg in range(filedoc.pageCount):
+        page = filedoc[pg]
+        rotate = int(0)
+        zoom_x = 2
+        zoom_y = 2
+        mat = fitz.Matrix(zoom_x, zoom_y).preRotate(rotate)
+        rect = page.rect
+        clip = fitz.Rect(rect.tl, rect.br)
+        pix = page.getPixmap(matrix=mat, alpha=False, clip=clip)
+
+        pix.writePNG(os.path.join(imagepath, '{}.png'.format(pg)))
+    filedoc.close()
+
+    zipdoc = zipfile.ZipFile(zippathname, 'w', zipfile.ZIP_DEFLATED)
+    for d in os.listdir(imagepath):
+        zipdoc.write(os.path.join(imagepath, d), os.path.split(d)[1], zipfile.ZIP_DEFLATED)
+    zipdoc.close()
+    shutil.rmtree(imagepath)
+    os.remove(pdfpathname)
+
+    return send_file(zippathname,
+                     mimetype='application/x-zip-compressed;charset=utf-8',
+                     attachment_filename=zipfilename,
+                     as_attachment=True)
+
+
 if __name__ == '__main__':
     app.run()
