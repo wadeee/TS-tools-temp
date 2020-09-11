245c95be6b3eafc046b9b0eefe916453579aaacc
wade <wadechen@outlook.com>
2020-09-11 17:58:01 +0800

    add linux part

---
+++ inst.sh
@@ -0,0 +1,14 @@
+## pip install ##
+python3 -m pip install flask
+python3 -m pip install jinja2
+python3 -m pip install gunicorn
+
+## add to service ##
+echo y | cp ./word2img.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable word2img
+systemctl start word2img
+
+## add config to nginx ##
+echo y | cp ./sscon.nginx.http.conf /etc/nginx/conf.d/
+systemctl restart nginx ## please make sure nginx installed ##
